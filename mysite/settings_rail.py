52cfb13ef75eac0da7bd503f8133f832401f4808
Arjun <arjunpk8921@gmail.com>
2023-06-25 20:10:57 +0530

    allowed host 2

--- mysite/settings_rail.py
+++ mysite/settings_rail.py
@@ -2,4 +2,4 @@ from mysite.settings import *
 from decouple import config
 
 SECRET_KEY = config('SECRET_KEY')
-ALLOWED_HOSTS = ['https://web-production-30e7.up.railway.app/']
+ALLOWED_HOSTS = ['web-production-30e7.up.railway.app']
